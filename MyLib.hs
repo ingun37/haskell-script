559129460c1e6a142ac43a7138babec11131c9d8
Ingun Jon <ingunjon@Inguns-MacBook-Pro.local>
2020-10-16 20:18:17 +0900

    /ib

--- MyLib.hs
+++ MyLib.hs
@@ -1,4 +1,5 @@
-module MyLib (someFunc) where
+module MyLib (onlyMatch) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- matchRegexPR gmatchRegexPR
+onlyMatch :: Functor m => m ((String, (String, String)), [(Int, String)]) -> m String
+onlyMatch = fmap (\((x,_),_) -> x)
