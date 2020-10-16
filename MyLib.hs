848910885c4f0d60d13e5ef8714b91e010b0ace5
Ingun Jon <ingun37@gmail.com>
2020-10-16 22:39:37 +0900

    refac

--- MyLib.hs
+++ MyLib.hs
@@ -13,7 +13,13 @@ onlyGroup :: Functor m => Int -> m ((String, (String, String)), [(Int, String)])
 onlyGroup groupIdx = fmap (!! groupIdx) . onlyGroups
 
 _allFiles :: FilePath -> IO [FilePath]
-_allFiles path = doesFileExist path >>= (\x -> if x then return [path] else allFiles path)
+_allFiles path = doesFileExist path >>= _ifInv (return [path]) (allFiles path)
 
 allFiles :: FilePath -> IO [FilePath]
-allFiles path = listDirectory path >>= fmap join . sequence . map (_allFiles . ((path ++ "/") ++))
+allFiles path = listDirectory path >>= _ioJoin . map (_allFiles . ((path ++ "/") ++))
+
+_ioJoin :: [IO[a]] -> IO [a]
+_ioJoin = fmap join . sequence
+
+_ifInv :: a -> a -> Bool -> a
+_ifInv a b c = if c then a else b
