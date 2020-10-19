9913a10a37542a681d2bd079c1d8139e67c7ec71
Ingun Jon <ingun37@gmail.com>
2020-10-19 22:10:12 +0900

    test

--- test/Test.hs
+++ test/Test.hs
@@ -1,10 +1,22 @@
 module Main where
 
 import Lib (parse)
-import System.Directory (listDirectory, removeDirectoryRecursive, createDirectoryIfMissing)
+import System.Directory (removeDirectoryRecursive, createDirectoryIfMissing)
+import Test.Hspec
+import Test.QuickCheck
+import qualified System.Directory.Tree as DT
+import qualified Data.ByteString as B
+
+toPath :: FilePath
+toPath = "test/data/to"
 
 main :: IO ()
-main = do
-  removeDirectoryRecursive "test/data/to"
-  createDirectoryIfMissing True "test/data/to"
-  parse ["test/data/from", "test/data/to"]
+main = hspec $ do
+  describe "Prelude.head" $ do
+    it "returns the first element of a list" $ do
+      removeDirectoryRecursive toPath
+      createDirectoryIfMissing True toPath
+      parse ["test/data/from", toPath]
+      (_ DT.:/ (DT.Dir _ toDT)) <- DT.readDirectoryWith B.readFile toPath
+      (_ DT.:/ (DT.Dir _ beDT)) <- DT.readDirectoryWith B.readFile "test/data/to-be-equal"
+      toDT `shouldBe` beDT
